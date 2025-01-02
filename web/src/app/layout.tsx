587b2e63f490dd777fd63905b0545b1378079721
imrannaazir <imrannaaziremon@gmail.com>
2025-01-02 19:02:25 +0600

    init : configured and intitialized next js project

---
+++ web/src/app/layout.tsx
@@ -0,0 +1,36 @@
+import { ThemeProvider } from "@/components/providers/theme-provider";
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "./globals.css";
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Programming Hero",
+  description:
+    "Learn and master programming to open a new career opportunity. Enjoy the most effective and fun way to learn programming as an absolute beginner.",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <ThemeProvider>{children}</ThemeProvider>
+      </body>
+    </html>
+  );
+}
